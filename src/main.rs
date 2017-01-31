dbf9ba4aead0f67024ecabd7378b02a18bb73884
akrimpenfort <akrimpenfort@uni-osnabrueck.de>
2017-02-01 02:29:28 +0900

    Remove unused fn in main
    It was a leftover from testing

--- src/main.rs
+++ src/main.rs
@@ -44,28 +44,3 @@ fn main() {
                 .expect("something went seriously wrong :O");
     log("Stop Analyzing Radio stations");
 }
-
-pub fn radio_antenne(html: &str) -> Option<String> {
-    if html.contains("\"total\":1") {
-        return None;
-    }
-    // General suffix for all fields
-    let suffix = "\",";
-    // Response contains two results, songs is saved in type PE_E
-    // It is not ensured that the PE_E is the always the first/second part of the response
-    let type_ident = "\"type\":\"PE_E\"";
-    let (_, html) = html.split_at(html.find(type_ident).unwrap() + type_ident.len());
-    // Artist
-    let artist_prefix = "\"artistName\":\"";
-    let a_pos = html.find(artist_prefix).unwrap();
-    let (_, artist) = html.split_at(a_pos + artist_prefix.len());
-    let a_pos = artist.find(suffix).unwrap();
-    let (artist , _) = artist.split_at(a_pos);
-    // Song
-    let song_prefix = "\"name\":\"";
-    let s_pos = html.find(song_prefix).unwrap();
-    let (_, song) = html.split_at(s_pos + song_prefix.len());
-    let s_pos = song.find(suffix).unwrap();
-    let (song , _) = song.split_at(s_pos);
-    Some(format!("{} - {}", artist, song))
-}
