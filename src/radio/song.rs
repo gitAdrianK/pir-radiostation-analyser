6c28870ad5f3d68f79ec87a7ea0cf6e6f007955d
akrimpenfort <akrimpenfort@uni-osnabrueck.de>
2017-02-02 21:07:20 +0900

    Add utility function, clean up functions in songs

--- src/radio/song.rs
+++ src/radio/song.rs
@@ -1,5 +1,4 @@
-// There has to be a way better, more canonical way to do this, but until
-// someone points it out, this works.
+use util::get_inbetween;
 
 #[derive(Debug, PartialEq)]
 pub struct Song {
@@ -36,45 +35,52 @@ pub fn radio_1live(html: &str) -> Option<Song> {
         || filter.contains("wir sind der westen")
         || filter.contains("echo des tages")
         || filter.contains("moderation:")
+        // Could be replace with a regex <smth>magazin
         || filter.contains("europamagazin")
+        || filter.contains("meinungsmagazin")
     {
         return None;
     }
-    match html.find("-") {
+    match html.find(" - ") {
         Some(pos) => {
             let (artist, title) = html.split_at(pos);
             return Some(
-                Song{ artist: artist.trim().into(), title: title.replace("-", "").trim().into() }
+                Song {
+                    artist: artist.trim().into(),
+                    title: title.replace(" - ", "").trim().into()
+                }
             );
         },
         None => {},
     }
     /// WDR 2 "von"
-    match html.find("von") {
+    match html.find(" von ") {
         Some(pos) => {
             let (title, artist) = html.split_at(pos);
             Some(
-                Song{ artist: artist.replace("von", "").trim().into(), title: title.trim().into() }
+                Song {
+                    artist: artist.replace(" von ", "").trim().into(),
+                    title: title.trim().into()
+                }
             )
         },
         None => None,
     }
 }
 
-//TODO: Rewrite into fewer repeating code line
-
 /// Song is after field "song_now"
 pub fn radio_ndr(html: &str) -> Option<Song> {
-    let prefix = "song_now\":  \"";
-    let suffix = "\",\n\"song_previous";
-    let s_pos = html.find(prefix).unwrap();
-    let (_, song) = html.split_at(s_pos + prefix.len());
-    let s_pos = song.find(suffix).unwrap();
-    let (song , _) = song.split_at(s_pos);
-    match song.find("-") {
+    let song = match get_inbetween(html, "song_now\":  \"", "\",\n\"song_previous") {
+        Some(song) => song,
+        None => return None,
+    };
+    match song.find(" - ") {
         Some(pos) => {
             let (artist, title) = song.split_at(pos);
-            Some(Song{ artist: artist.trim().into(), title: title.replace("-", "").trim().into() })
+            Some( Song {
+                artist: artist.trim().into(),
+                title: title.replace(" - ", "").trim().into()
+            })
         },
         None => None,
     }
@@ -84,23 +90,21 @@ pub fn radio_antenne(html: &str) -> Option<Song> {
     if html.contains("\"total\":1") {
         return None;
     }
-    // General suffix for all fields
-    let suffix = "\",";
     // Response contains two results, songs is saved in type PE_E
     // It is not ensured that the PE_E is the always the first/second part of the response
-    let type_ident = "\"type\":\"PE_E\"";
-    let (_, html) = html.split_at(html.find(type_ident).unwrap() + type_ident.len());
+    let html = match get_inbetween(html, "\"type\":\"PE_E\"", ":true") {
+        Some(html) => html,
+        None => return None,
+    };
     // Artist
-    let artist_prefix = "\"artistName\":\"";
-    let a_pos = html.find(artist_prefix).unwrap();
-    let (_, artist) = html.split_at(a_pos + artist_prefix.len());
-    let a_pos = artist.find(suffix).unwrap();
-    let (artist , _) = artist.split_at(a_pos);
+    let artist = match get_inbetween(&html, "\"artistName\":\"", "\",\"song\"") {
+        Some(artist) => artist,
+        None => return None,
+    };
     // Song
-    let song_prefix = "\"name\":\"";
-    let s_pos = html.find(song_prefix).unwrap();
-    let (_, song) = html.split_at(s_pos + song_prefix.len());
-    let s_pos = song.find(suffix).unwrap();
-    let (song , _) = song.split_at(s_pos);
+    let song = match get_inbetween(&html, "\"name\":\"", "\"artistName\":\"") {
+        Some(song) => song,
+        None => return None,
+    };
     Some(Song{ artist: artist.into(), title: song.into() })
 }
--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,2 +1,23 @@
 pub mod logger;
 pub mod http_scraper;
+
+/// Given a prefix and a suffix returns everything in-between.
+/// If prefix or suffix aren't contained None will be returned.
+/// Should the prefix appear before the suffix, or the text between those be empty
+/// None will be returned.
+pub fn get_inbetween(string: &str, prefix: &str, suffix: &str) -> Option<String> {
+    let beginn = match string.find(prefix) {
+        Some(pos) => pos + prefix.len(),
+        None => return None,
+    };
+    let end = match string.find(suffix) {
+        Some(pos) => pos,
+        None => return None,
+    };
+    if beginn >= end {
+        return None;
+    }
+    unsafe {
+        Some(string.slice_unchecked(beginn, end).into())
+    }
+}
